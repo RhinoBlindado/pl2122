82fcc75f5bc0e29e39f4c626445c49e657b595c8
Valentino Lugli <valentinolugli@gmail.com>
2021-12-02 09:50:36 +0100

    [P5][0%] Initial push for P5

---
+++ practica5/symbolTable.h
@@ -0,0 +1,70 @@
+#include <string.h>
+#ifndef SYMBOL_TABLE
+
+    #define SYMBOL_TABLE
+
+    typedef enum
+    {
+        EMPTY,
+        BLOCK_START,   // Marca para señalar los inicios de un nuevo bloque o cambio de scope
+        FUNC,          // Indica que la entrada a la tabla de simbolos es una funcion
+        VARIABLE,      // Indica que la entrada a la tabla de simbolos es una variable
+        PARAMETER      // Indica que la entrada a la tabla de simbolos es un parámetro
+    } typeInput;
+
+    typedef enum
+    {
+        SUS,         // = null. Valor por defecto
+        ENTERO,
+        REAL,
+        CARACTER,
+        BOOLEANO,
+        LISTA,
+        NO_ASIGNADO  // Variable sin tipo asignado (declaración)
+    } dType;
+
+    char* getStr(dType t) {
+        char* res = (char*) malloc(50);
+        switch (t) {
+            case (SUS):
+                strcpy(res, "SUS");
+            break;
+            case (ENTERO):
+                strcpy(res, "ENTERO");
+            break;
+            case (REAL):
+                strcpy(res, "REAL");
+            break;
+            case (CARACTER):
+                strcpy(res, "CARACTER");
+            break;
+            case (BOOLEANO):
+                strcpy(res, "BOOLEANO");
+            break;
+            case (LISTA):
+                strcpy(res, "LISTA");
+            break;
+            case (NO_ASIGNADO):
+                strcpy(res, "NO_ASIGNADO");
+            break;
+    }
+
+    return res;
+    }
+
+    typedef struct
+    {
+        // Tipo de entrada en la tabla de símbolos
+        typeInput input;
+        // Nombre del lexema
+        char* name;
+        // Tipo de datos
+        dType dataType;
+        // Numero de datos de la funcion
+        uint params;
+        // Elementos para lista
+        dType listDataType;
+    } symbolTable;
+
+
+#endif
