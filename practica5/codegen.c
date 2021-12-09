05347ddcc5c553a4dbe4d61790acd292b253ccfc
Sergio Guaka <scm0@correo.ugr.es>
2021-12-09 09:23:21 +0100

    [P5] Fix anunciar no longer writes \n at the end

--- practica5/codegen.c
+++ practica5/codegen.c
@@ -383,15 +383,15 @@ char* getPrint(attr exp)
 		
 
 	if(exp.type == ENTERO)
-		sprintf(res, "%s%sprintf(\"%sd\\n\", %s);\n", res, getTabs(), "%", varSalida); 
+		sprintf(res, "%s%sprintf(\"%sd\", %s);\n", res, getTabs(), "%", varSalida);
 	else if(exp.type == REAL)
-		sprintf(res, "%s%sprintf(\"%slf\\n\", %s);\n", res, getTabs(), "%", varSalida); 
+		sprintf(res, "%s%sprintf(\"%slf\", %s);\n", res, getTabs(), "%", varSalida);
 	else if(exp.type == CARACTER && exp.isList == 0)
-		sprintf(res, "%s%sprintf(\"%sc\\n\", %s);\n", res, getTabs(), "%", varSalida); 
+		sprintf(res, "%s%sprintf(\"%sc\", %s);\n", res, getTabs(), "%", varSalida);
 	else if(exp.type == BOOLEANO)
-		sprintf(res, "%s%sprintf(\"%sd\\n\", %s);\n", res, getTabs(), "%", varSalida);
+		sprintf(res, "%s%sprintf(\"%sd\", %s);\n", res, getTabs(), "%", varSalida);
 	else if (exp.type == CARACTER && exp.isList == 1)
-		sprintf(res, "%s%sprintf(\"%ss\\n\", %s);\n", res, getTabs(), "%", varSalida);
+		sprintf(res, "%s%sprintf(\"%ss\", %s);\n", res, getTabs(), "%", varSalida);
 
 	return res;
 }
