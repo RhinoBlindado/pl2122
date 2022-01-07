7d511d70840fe87d22d22e7efadb4ed8cc51954d
Sergio Guaka <scm0@correo.ugr.es>
2022-01-07 12:48:17 +0100

    Entrega

--- practica5/output.c
+++ practica5/output.c
@@ -3,60 +3,94 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h>
-#include "dec_fun.c"
+#include "dec_func.h"
+#include "dec_dat.h"
 
-int ve, ve2;
-double vf;
-char vc, v2, v4;
-bool vl, vl2, vl3, vl4;
+int a, b, c, cont;
+l_int lista1, lista2, lista3, listaAux1, listaAux2;
 
 int main()
 {
+	printf("%s", "Ingresa un numero:\n>");
+	scanf("%d", &a);
 	{
-		int tmp1;
-		tmp1 = pow(3, 2);
+		b = pares(a);
+	}
+	{
+		b = impares(a);
+	}
+	{
+		b = imprimir_lista();
+	}
+	{
+		l_int tmp21;
+		int_sumarLista(&lista1,3);int_copiar(&lista1,&tmp21);
 
-		int tmp2;
-		tmp2 = 4 * tmp1;
+		lista3 = tmp21;
+	}
+	printf("%s", "sumando tres a pares: \n");
+	{
+		int tmp22;
+		tmp22 = int_longitud(&lista3);
 
-		ve = tmp2;
+		cont = tmp22;
 	}
 	{
-		vf = 3.5;
+		c = 1;
 	}
-	if(!true) goto etiqueta2;
+	etiqueta12:
+	if(c > cont) goto etiqueta13;
 	{
+		int tmp24;
 		{
-			ve = 7;
+			int tmp23;
+			tmp23 = int_consultarEnPosicion(lista3, c);
+
+			tmp24 = tmp23;
 		}
-		if(!true) goto etiqueta1;
-		{
-			{
-				ve = 1;
-			}
-			{
-				vf = 4.33;
-			}
-			{
-				vf = 7.44;
-			}
-			if(!false) goto etiqueta0;
-			{
-				{
-					ve = 4444;
-				}
-			}
-			etiqueta0:
-			{
-			}
-		}
-		etiqueta1:
-		{
-		}
+		printf("%d", tmp24);
+		printf("%s", ", ");
 	}
-	etiqueta2:
+	c += 1;
+	goto etiqueta12;
+	etiqueta13:
 	{
 	}
-	printf("%d\n", ve);
-	printf("%lf\n", vf);
+	printf("%s", "\n");
+	{
+		l_int tmp25;
+		int_multiplicarLista(&lista1,2);
+int_copiar(&lista1,&tmp25);
+
+		lista3 = tmp25;
+	}
+	printf("%s", "multiplicando pares por dos: \n");
+	{
+		int tmp26;
+		tmp26 = int_longitud(&lista3);
+
+		cont = tmp26;
+	}
+	{
+		c = 1;
+	}
+	etiqueta14:
+	if(c > cont) goto etiqueta15;
+	{
+		int tmp28;
+		{
+			int tmp27;
+			tmp27 = int_consultarEnPosicion(lista3, c);
+
+			tmp28 = tmp27;
+		}
+		printf("%d", tmp28);
+		printf("%s", ", ");
+	}
+	c += 1;
+	goto etiqueta14;
+	etiqueta15:
+	{
+	}
+	printf("%s", "\n");
 }
